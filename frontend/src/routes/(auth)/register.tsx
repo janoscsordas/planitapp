8b4f6596b569a2aaee4ba3ca384e3d3c7879e959
János Csordás <janoscsordas072@gmail.com>
2026-04-19 16:29:22 +0200

    chore: added better UI for register page

--- frontend/src/routes/(auth)/register.tsx
+++ frontend/src/routes/(auth)/register.tsx
@@ -1,13 +1,12 @@
 import { createFileRoute, Link } from '@tanstack/react-router'
 import { useForm } from '@tanstack/react-form'
 import z from 'zod'
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '#/components/ui/card'
-import { FieldGroup } from '#/components/ui/field'
+import { Card, CardContent } from '#/components/ui/card'
+import { FieldDescription, FieldGroup, FieldSeparator } from '#/components/ui/field'
 import { Field, FieldLabel, FieldError } from '#/components/ui/field'
 import { Input } from '#/components/ui/input'
 import { IconBrandGithub } from "@tabler/icons-react"
 import { Button } from '#/components/ui/button'
-import { Separator } from '#/components/ui/separator'
 import { authClient } from '#/lib/auth-client'
 import OAuthButton from '#/components/auth/oauth-button'
 import { Spinner } from '#/components/ui/spinner'
@@ -44,6 +43,8 @@ function RegisterPage() {
       onSubmit: registerFormSchema,
     },
     onSubmit: async ({ value }) => {
+      console.log("HELLO")
+
       const { error } = await authClient.signUp.email({
         name: value.email.split('@')[0],
         email: value.email,
@@ -66,129 +67,143 @@ function RegisterPage() {
   })
 
   return (
-    <main className='w-full min-h-screen flex justify-center items-center'>
-      <Card className="w-full sm:max-w-md">
-        <CardHeader>
-          <CardTitle>Fiók Regisztráció</CardTitle>
-          <CardDescription>
-            Regisztrálj fiókot az alkalmazás használatához
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form
-            onSubmit={(e) => {
-              e.preventDefault()
-              form.handleSubmit()
-            }}
-            id="register-form"
-          >
-            <FieldGroup>
-              <form.Field
-                name="email"
-                children={(field) => {
-                  const isInvalid =
-                    field.state.meta.isTouched && !field.state.meta.isValid
-                  return (
-                    <Field data-invalid={isInvalid}>
-                      <FieldLabel htmlFor={field.name}>Email cím</FieldLabel>
-                      <Input
-                        id={field.name}
-                        name={field.name}
-                        value={field.state.value}
-                        onBlur={field.handleBlur}
-                        onChange={(e) => field.handleChange(e.target.value)}
-                        aria-invalid={isInvalid}
-                        type="email"
-                        placeholder="Email"
-                        autoComplete="off"
-                      />
-                      {isInvalid && (
-                        <FieldError errors={field.state.meta.errors} />
-                      )}
-                    </Field>
-                  )
+    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-4xl">
+        <div className="flex flex-col gap-6">
+          <Card className="overflow-hidden p-0">
+            <CardContent className="grid p-0 md:grid-cols-2">
+              <form 
+                onSubmit={(e) => {
+                  e.preventDefault()
+                  form.handleSubmit()
                 }}
-              />
-              <form.Field
-                name="password"
-                children={(field) => {
-                  const isInvalid =
-                    field.state.meta.isTouched && !field.state.meta.isValid
-                  return (
-                    <Field data-invalid={isInvalid}>
-                      <FieldLabel htmlFor={field.name}>Jelszó</FieldLabel>
-                      <Input
-                        id={field.name}
-                        name={field.name}
-                        value={field.state.value}
-                        onBlur={field.handleBlur}
-                        type='password'
-                        onChange={(e) => field.handleChange(e.target.value)}
-                        placeholder="**********"
-                        aria-invalid={isInvalid}
-                      />
-                      {isInvalid && (
-                        <FieldError errors={field.state.meta.errors} />
-                      )}
-                    </Field>
-                  )
-                }}
-              />
-              <form.Field
-                name="passwordConfirm"
-                children={(field) => {
-                  const isInvalid =
-                    field.state.meta.isTouched && !field.state.meta.isValid
-                  return (
-                    <Field data-invalid={isInvalid}>
-                      <FieldLabel htmlFor={field.name}>Jelszó megerősítése</FieldLabel>
-                      <Input
-                        id={field.name}
-                        name={field.name}
-                        value={field.state.value}
-                        onBlur={field.handleBlur}
-                        type='password'
-                        onChange={(e) => field.handleChange(e.target.value)}
-                        placeholder="**********"
-                        aria-invalid={isInvalid}
-                      />
-                      {isInvalid && (
-                        <FieldError errors={field.state.meta.errors} />
-                      )}
-                    </Field>
-                  )
-                }}
-              />
-            </FieldGroup>
-          </form>
-        </CardContent>
-        <CardFooter className='flex flex-col gap-5'>
-          <form.Subscribe 
-            selector={(state) => [state.canSubmit, state.isSubmitting]}
-            children={([, isSubmitting]) => (
-              <Field orientation="horizontal">
-                <Button className="w-full" type="submit" form="register-form" disabled={isSubmitting}>
-                  {isSubmitting ? <Spinner /> : 'Regisztráció'}
-                </Button>
-              </Field>
-            )}
-          />
-          <div className="relative w-full">
-            <Separator />
-            <p className="absolute z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-muted font-medium px-3 text-center text-xs text-muted-foreground">
-              VAGY
-            </p>
-          </div>
+                className="p-6 md:p-8"
+                id="register-form"
+              >
+                <FieldGroup>
+                  <div className="flex flex-col items-center gap-2 text-center">
+                    <h1 className="text-2xl font-bold">Fiók Regisztráció</h1>
+                    <p className="text-sm text-balance text-muted-foreground">
+                      Adja meg az e-mail címét a fiók létrehozásához
+                    </p>
+                  </div>
+                  <form.Field
+                    name="email"
+                    children={(field) => {
+                      const isInvalid =
+                        field.state.meta.isTouched && !field.state.meta.isValid
+                      return (
+                        <Field data-invalid={isInvalid}>
+                          <FieldLabel htmlFor={field.name}>Email cím</FieldLabel>
+                          <Input
+                            id={field.name}
+                            name={field.name}
+                            value={field.state.value}
+                            onBlur={field.handleBlur}
+                            onChange={(e) => field.handleChange(e.target.value)}
+                            aria-invalid={isInvalid}
+                            type="email"
+                            placeholder="m@example.com"
+                            autoComplete="off"
+                          />
+                          {isInvalid && (
+                            <FieldError errors={field.state.meta.errors} />
+                          )}
+                        </Field>
+                      )
+                    }}
+                  />
+                  <form.Field
+                    name="password"
+                    children={(field) => {
+                      const isInvalid =
+                        field.state.meta.isTouched && !field.state.meta.isValid
+                      return (
+                        <Field data-invalid={isInvalid}>
+                          <FieldLabel htmlFor={field.name}>Jelszó</FieldLabel>
+                          <Input
+                            id={field.name}
+                            name={field.name}
+                            value={field.state.value}
+                            onBlur={field.handleBlur}
+                            onChange={(e) => field.handleChange(e.target.value)}
+                            aria-invalid={isInvalid}
+                            type="password"
+                            placeholder="**********"
+                            autoComplete="off"
+                          />
+                          {isInvalid && (
+                            <FieldError errors={field.state.meta.errors} />
+                          )}
+                        </Field>
+                      )
+                    }}
+                  />
+                  <form.Field
+                    name="passwordConfirm"
+                    children={(field) => {
+                      const isInvalid =
+                        field.state.meta.isTouched && !field.state.meta.isValid
+                      return (
+                        <Field data-invalid={isInvalid}>
+                          <FieldLabel htmlFor={field.name}>Jelszó megerősítése</FieldLabel>
+                          <Input
+                            id={field.name}
+                            name={field.name}
+                            value={field.state.value}
+                            onBlur={field.handleBlur}
+                            type='password'
+                            onChange={(e) => field.handleChange(e.target.value)}
+                            placeholder="**********"
+                            aria-invalid={isInvalid}
+                          />
+                          {isInvalid && (
+                            <FieldError errors={field.state.meta.errors} />
+                          )}
+                        </Field>
+                      )
+                    }}
+                  />
+                  <form.Subscribe 
+                    selector={(state) => [state.canSubmit, state.isSubmitting]}
+                    children={([, isSubmitting]) => (
+                      <Field orientation="horizontal">
+                        <Button className="w-full cursor-pointer" type="submit" form="register-form" disabled={isSubmitting}>
+                          {isSubmitting ? <Spinner /> : 'Regisztráció'}
+                        </Button>
+                      </Field>
+                    )}
+                  />
+                  <FieldSeparator className="*:data-[slot=field-separator-content]:bg-card">
+                    Vagy az alábbiakkal
+                  </FieldSeparator>
 
-          {/* GitHub OAuth button */}
-          <OAuthButton provider={{ id: 'github', name: 'Folytatás GitHub fiókkal', icon: <IconBrandGithub /> }} />
+                  <Field className="flex flex-col gap-4">
+                    <OAuthButton provider={{ id: "github", name: "Folytatás GitHub fiókkal", icon: <IconBrandGithub /> }} />
+                  </Field>
+                  
+                  <FieldDescription className="text-center">
+                    Már van fiókod? <Link to="/login">Jelentkezz be!</Link>
+                  </FieldDescription>
+                </FieldGroup>
+              </form>
 
-          <Separator />
-          <p className="text-center text-xs text-muted-foreground">
-            Már van fiókod? <Link to="/login" className='underline'>Jelentkezz be</Link>
-          </p>
-        </CardFooter>
-      </Card>
-    </main>
+              {/* Placeholder Image */}
+
+              <div className="relative hidden bg-muted md:block">
+                <img
+                  src="/placeholder.svg"
+                  alt="Image"
+                  className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+                />
+              </div>
+            </CardContent>
+          </Card>
+          <FieldDescription className="px-6 text-center">
+            A folytatáshoz elfogadom a <Link to="/">Felhasználói Feltételeket</Link> és az <Link to="/">Adatvédelmi Szabályzatot</Link>.
+          </FieldDescription>
+        </div>
+      </div>
+    </div>
   )
 }
